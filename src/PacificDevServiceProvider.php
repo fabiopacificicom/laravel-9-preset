cbec4862c9e0c06500bbb7699bc8fc51547d8ee7
fbhood <mail@fabiopacifici.com>
2022-10-15 09:59:37 +0200

    First draft

---
+++ src/PacificDevServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PacificDev\Laravel9Preset;
+
+use Illuminate\Support\ServiceProvider;
+use PacificDev\Laravel9Preset\Commands\Preset;
+
+class PacificDevServiceProvider extends ServiceProvider
+{
+   
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Preset::class,
+            ]);
+        }
+    }
+}
