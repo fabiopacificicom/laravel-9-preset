6a1b3c2b1da3d7be5cb4d41cbca6de27721baa21
fbhood <mail@fabiopacifici.com>
2022-11-01 14:33:13 +0100

    refactor default preset into a dedicated class
    Refactored all the logic inside the Preset class into
    a dedicated UICommand class that triggers only the
    default preset updates.

--- src/Commands/Preset.php
+++ src/Commands/Preset.php
@@ -3,7 +3,7 @@
 namespace PacificDev\Laravel9Preset\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\File;
+use InvalidArgumentException;
 
 class Preset extends Command
 {
@@ -12,7 +12,10 @@ class Preset extends Command
      *
      * @var string
      */
-    protected $signature = 'preset {bootstrap}';
+    protected $signature = 'preset:ui
+    { type : The preset type (bootstrap) }
+    { --auth : Installs the authentication scaffolding }
+    ';
 
     /**
      * The console command description.
@@ -28,56 +31,31 @@ class Preset extends Command
      */
     public function handle()
     {
-        $this->info('setting up bootstrap and vite');
-        self::install();
-        $this->comment('Now you can run: npm i && npm run dev');
+
+        if ($this->argument('type') === 'bootstrap' && !$this->option('auth')) {
+
+            $this->bootstrap();
+        }
+        if ($this->argument('type') === 'bootstrap' && $this->option('auth')) {
+            # Install the auth preset - run preset:auth
+            $this->authentication();
+        }
+        if ($this->argument('type') !== 'bootstrap') {
+            # Invalid preset
+            throw new InvalidArgumentException('This preset is invalid. Presets available: bootstrap');
+        }
     }
 
-    public static function install()
+    public function bootstrap()
     {
-        self::update_css();
-        // remove postCss from package.json
-        self::update_vite_config();
-        // update js/css stubs
-        self::update_app_js();
-        // update welcome view
-        self::add_welcome_page();
-        // update packages
-        self::update_packages();
+        Presets\UICommand::install();
+        $this->info('Bootstrap, Sass, and Vite setup successful');
+        $this->warn('Now you can run: [npm i && npm run dev] to compile all assets');
     }
-
-    public static function update_css()
+    public function authentication()
     {
-        //$this->info('folder css renamed as scss');
-        File::moveDirectory(resource_path('css'), resource_path('scss'));
-        //$this->info('scss file added');
-        File::copy(__DIR__.'/../stubs/app.scss', resource_path('scss/app.scss'));
-        //File:copy(resource_path('scss/app.css'), resource_path('scss/app.scss'));
-    }
-
-    public static function update_vite_config()
-    {
-        //$this->info('Vite config file created');
-        File::copy(__DIR__.'/../stubs/vite.config.js', base_path('vite.config.js'));
-    }
-
-    public static function update_app_js()
-    {
-        //$this->info('Updating js file');
-        File::copy(__DIR__.'/../stubs/app.js', resource_path('js/app.js'));
-    }
-
-    public static function update_packages()
-    {
-        //$this->info('Updating package.json');
-        File::copy(__DIR__.'/../stubs/package.json', base_path('package.json'));
-        //$this->warn('Now you can run: npm i && npm run dev');
-    }
-
-    public static function add_welcome_page()
-    {
-        //$this->info('Adding welcome view');
-        File::copy(__DIR__.'/../stubs/welcome.blade.php', resource_path('views/welcome.blade.php'));
+        Presets\AuthCommand::install();
+        $this->info('Bootstrap, Sass, and Vite Authentication scaffolding setup successful');
+        $this->warn('Now you can run: [npm i && npm run dev] to compile all assets');
     }
 }
-//
---
+++ src/Commands/Presets/UICommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace PacificDev\Commands\Presets;
+
+use PacificDev\Commands\Preset;
+use Illuminate\Support\Facades\File;
+
+class UICommand extends Preset
+{
+
+    public static function install()
+    {
+        self::update_css();
+        // remove postCss from package.json
+        self::update_vite_config();
+        // update js/css stubs
+        self::update_app_js();
+        // update welcome view
+        self::add_welcome_page();
+        // update packages
+        self::update_packages();
+    }
+
+    public static function update_css()
+    {
+        //$this->info('folder css renamed as scss');
+        File::moveDirectory(resource_path('css'), resource_path('scss'));
+        //$this->info('scss file added');
+        File::copy(__DIR__ . '/../../stubs/app.scss', resource_path('scss/app.scss'));
+        //File:copy(resource_path('scss/app.css'), resource_path('scss/app.scss'));
+    }
+
+    public static function update_vite_config()
+    {
+        //$this->info('Vite config file created');
+        File::copy(__DIR__ . '/../../stubs/vite.config.js', base_path('vite.config.js'));
+    }
+
+    public static function update_app_js()
+    {
+        //$this->info('Updating js file');
+        File::copy(__DIR__ . '/../../stubs/app.js', resource_path('js/app.js'));
+    }
+
+    public static function update_packages()
+    {
+        //$this->info('Updating package.json');
+        File::copy(__DIR__ . '/../../stubs/package.json', base_path('package.json'));
+        //$this->warn('Now you can run: npm i && npm run dev');
+    }
+
+    public static function add_welcome_page()
+    {
+        //$this->info('Adding welcome view');
+        File::copy(__DIR__ . '/../../stubs/welcome.blade.php', resource_path('views/welcome.blade.php'));
+    }
+}
