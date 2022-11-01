5e14b58398c13a4d502ed86238fb765acd1ee526
fbhood <mail@fabiopacifici.com>
2022-11-01 15:37:36 +0100

    fix template

--- src/stubs/welcome.blade.php
+++ src/stubs/welcome.blade.php
@@ -11,22 +11,9 @@
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
-    <!DOCTYPE html>
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    @vite('resources/js/app.js')
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        @vite('resources/js/app.js')
-
-    </head>
+</head>
 
 <body>
 
