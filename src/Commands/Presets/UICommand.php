e4eed3b7868502978a7d084655dfb8f73bbcf503
Donnie <donniepask@gmail.com>
2022-11-04 10:27:42 +0100

    Fixed dependencies updates

--- src/Commands/Presets/UICommand.php
+++ src/Commands/Presets/UICommand.php
@@ -18,7 +18,8 @@ class UICommand extends Preset
         // update welcome view
         self::add_welcome_page();
         // update packages
-        self::update_packages();
+        static::update_packages('dependencies');
+        static::update_packages('devDependencies');
     }
 
     public static function update_css()
@@ -42,11 +43,33 @@ class UICommand extends Preset
         File::copy(__DIR__ . '/../../stubs/app.js', resource_path('js/app.js'));
     }
 
-    public static function update_packages()
+    // public static function update_packages()
+    // {
+    //     //$this->info('Updating package.json');
+    //     File::copy(__DIR__ . '/../../stubs/package.json', base_path('package.json'));
+    //     //$this->warn('Now you can run: npm i && npm run dev');
+    // }
+
+    protected static function update_package_array($packages, $configuration_key)
     {
-        //$this->info('Updating package.json');
-        File::copy(__DIR__ . '/../../stubs/package.json', base_path('package.json'));
-        //$this->warn('Now you can run: npm i && npm run dev');
+        $package_array = [];
+
+        if ($configuration_key === 'dependencies') {
+            $package_array = [
+                "@popperjs/core" => "^2.11.6",
+                "bootstrap" => "^5.2.2"
+            ];
+        } else {
+            $package_array = [
+                "axios" => "^0.27",
+                "laravel-vite-plugin" => "^0.6.0",
+                "lodash" => "^4.17.19",
+                "sass" => "^1.55.0",
+                "vite" => "^3.0.0"
+            ];
+        }
+
+        return $package_array + $packages;
     }
 
     public static function add_welcome_page()
