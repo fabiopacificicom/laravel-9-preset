c6501bed1f5c4c4f421d551856d772776fd35cc5
fbhood <mail@fabiopacifici.com>
2023-02-17 17:37:49 +0100

    fix typo and improve welcome message close #16

--- src/stubs/auth/views/welcome.blade.php
+++ src/stubs/auth/views/welcome.blade.php
@@ -11,11 +11,11 @@
             </svg>
         </div>
         <h1 class="display-5 fw-bold">
-            Welcome to Laravel+Bootsrap 5
+            Welcome to Laravel+Bootstrap 5
         </h1>
 
-        <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-        <button class="btn btn-primary btn-lg" type="button">Example button</button>
+        <p class="col-md-8 fs-4">This a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 10.x</p>
+        <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Documentation</a>
     </div>
 </div>
 
